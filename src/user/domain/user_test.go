2d584e38a38a93357ff859950cc1cd67b40adbd2
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-03-20 09:40:22 +0900

    Test UserCreate

--- src/user/domain/user_test.go
+++ src/user/domain/user_test.go
@@ -10,12 +10,31 @@ func TestUserNewCreate(t *testing.T) {
 	userName := "신영현"
 	userPassword := "비밀"
 
-	user := UserNewCreate(UserNewProps{
+	user := UserNewCreate(UserNewProps {
 		userId:   userId,
 		name:     userName,
 		password: userPassword,
 	})
 
+	assert.Equal(t, user.userId.value, userId)
+	assert.Equal(t, user.name.value, userName)
+	assert.Equal(t, user.password.value, userPassword)
+}
+
+func TestUserCreate(t *testing.T) {
+	id := "abcdefg"
+	userId := "shindevil1@naver.com"
+	userName := "신영현"
+	userPassword := "비밀"
+
+	user := UserCreate(UserProps {
+		id:       id,
+		userId:   userId,
+		name:     userName,
+		password: userPassword,
+	})
+
+	assert.Equal(t, user.id, id)
 	assert.Equal(t, user.userId.value, userId)
 	assert.Equal(t, user.name.value, userName)
 	assert.Equal(t, user.password.value, userPassword)
